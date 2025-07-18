ba6cbc760bd450388df309cb379cef856ed4e470
Catalin <catalin-george-adrian.otet@endava.com>
2025-07-18 10:27:45 +0300

    Added a log database

--- Python-OOP-HMWRK/api_methods.py
+++ Python-OOP-HMWRK/api_methods.py
@@ -1,7 +1,37 @@
 from flask import Flask, jsonify, request, abort
 from math_functions import pow, fibonacci, factorial
+import sqlite3
 
 app = Flask(__name__)
+
+DB_PATH = 'api_log.db'
+
+def init_db():
+    """Initialize the DB and create the log table if it doesn't exist."""
+    with sqlite3.connect(DB_PATH, timeout=5) as conn:
+        conn.execute('''
+          CREATE TABLE IF NOT EXISTS api_log (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                method TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                parameters TEXT NOT NULL
+              );
+              '''
+        )
+
+def log_usage(method, **kwargs):
+    """ 
+    Insert a row into usage log. 
+    Methods are pow, fibonacci or factorial.
+    kwargs can include base, exponent or n as appropiate
+    """
+    parameters_str = ','.join(f"{key}={value}" for key, value in kwargs.items())
+    with sqlite3.connect(DB_PATH, timeout=5) as conn:
+        conn.execute(
+          "INSERT INTO api_log (method, parameters) VALUES (?, ?)",
+          (method, parameters_str)
+        )
+
 
 @app.route('/pow', methods=['GET'])
 def pow_route():
@@ -12,6 +42,8 @@ def pow_route():
         abort(400, description="Base and exponent are required to perform power op")
 
     result = pow(base, exponent)
+
+    log_usage("pow", base=base, exponent=exponent)
 
     return jsonify({
         'base': base,
@@ -27,6 +59,8 @@ def fibonacci_route():
         abort(400, description="n is required to calculate fibonacci sequence")
 
     result = fibonacci(n)
+
+    log_usage("fibonacci", n=n)
 
     return jsonify({
         'n': n,
@@ -45,10 +79,13 @@ def factorial_route():
     except ValueError as e:
         abort(400, description=str(e))
 
+    log_usage("factorial", n=n)
+
     return jsonify({
         'n': n,
         'result': result
     })
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
